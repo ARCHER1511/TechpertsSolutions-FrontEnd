be37b005bfebd9291a884eed282ec1568d054e9c
Ramy Alaa Mahana <ramymahana7@gmail.com>
2025-07-30 15:49:31 +0300

    slider in homw page

--- src/app/components/home/components/home-slider/home-slider.component.ts
+++ src/app/components/home/components/home-slider/home-slider.component.ts
@@ -1,37 +1,13 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, Renderer2, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-slider',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home-slider.component.html',
   styleUrl: './home-slider.component.css'
 })
-export class HomeSliderComponent implements AfterViewInit {
-  
-  @ViewChild('carouselExample') carouselRef!: ElementRef;
-
-  constructor(
-    private renderer: Renderer2,
-    @Inject(PLATFORM_ID) private platformId: Object
-  ) {}
-
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      const bootstrap = (window as any).bootstrap;
-      const carouselElement = this.carouselRef.nativeElement;
-
-      if (carouselElement && bootstrap?.Carousel) {
-        new bootstrap.Carousel(carouselElement, {
-          interval: 3000,
-          wrap: true,
-          pause: false
-        });
-      } else {
-        console.warn('Bootstrap is not available');
-      }
-    }
-  }
+export class HomeSliderComponent {
+  // Bootstrap carousel will be initialized automatically via data-bs-ride
 }
